8ade8f88d4712dd278c0a885b623bcbb24a8416c
Md Tofajjal Hossen <tofajjalhossen14@gmail.com>
2023-12-17 23:22:09 +0600

    Full Design upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+$(function(){
+
+	$('#Container').mixItUp();
+ /* $('.image-link').magnificPopup({type:'image'}); */
+ $('.image-link').magnificPopup({
+  type: 'image',
+  gallery:{
+    enabled:true
+  }
+});
+});
